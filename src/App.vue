12552481a95eefdf4fb28b71721a57b829b1ec17
ker-svg <kerem224koc@gmail.com>
2024-11-14 16:04:29 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <BattlePage></BattlePage>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import BattlePage from "./BattlePage.vue";
+</script>
